54a98b67bbeb59db27e3f950c785c0c67183c276
nvant001 <nvant001@odu.edu>
2024-04-05 19:18:31 -0400

    user_clicks post

--- userdata/user_clicks.py
+++ userdata/user_clicks.py
@@ -6,3 +6,9 @@ from userdata.models import UserClicks, UserClick_schema, UserClicks_schema
 def show_all():
     clicks = UserClicks.query.all()
     return UserClicks_schema.dump(clicks)
+
+def add(entry):
+    new_item = UserClick_schema.load(entry, session=db.session)
+    db.session.add(new_item)
+    db.session.commit()
+    return UserClick_schema.dump(new_item), 201
